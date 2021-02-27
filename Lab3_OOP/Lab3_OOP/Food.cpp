50e14c3763942a9881b2924efbe7e3f8cc938e06
Matwey Ivanov <matveykai@list.ru>
2021-02-27 14:19:26 +0300

    Added food class

---
+++ Lab3_OOP/Lab3_OOP/Food.cpp
@@ -0,0 +1,68 @@
+#include "Food.h"
+
+int Food::foodCount = 0;
+
+Food::Food(double price, FoodType type, double weight, int count, Date* produce_date, string* name)
+{
+	if (price < 0 || weight < 0)
+		throw exception("Wrong arguments");
+
+	this->price = price;
+	this->type = type;
+	this->weight = weight;
+	this->count = count;
+	this->produceDate = produce_date;
+	this->name = name;
+
+	if (name != nullptr)
+		Food::foodCount++;
+}
+Food::Food(const Food& obj)
+{
+	this->price = obj.price;
+	this->type = obj.type;
+	this->weight = obj.weight;
+	this->count = obj.count;
+	this->produceDate = new Date(*obj.produceDate);
+	this->name = new string(*obj.name);
+}
+
+double Food::get_price() { return price; }
+FoodType Food::get_type() { return type; }
+double Food::get_weight() { return weight; }
+int Food::get_count() { return count; }
+Date* Food::get_date() { return new Date(*produceDate); }
+string* Food::get_name() { return new string(*name); }
+int Food::get_food_count() { return foodCount; }
+void Food::destroy(bool copy) 
+{
+	if (!copy)
+		Food::foodCount--;
+	this->~Food();
+}
+Food::~Food() 
+{
+	if (produceDate != nullptr)
+		delete produceDate;
+	if (name != nullptr)
+		delete name;
+}
+
+bool Food::operator==(Food obj) 
+{
+	return price == obj.price
+		&& type == obj.type
+		&& produceDate == obj.produceDate
+		&& name == obj.name;
+}
+Food& Food::operator =(const Food& obj) 
+{
+	this->price = obj.price;
+	this->type = obj.type;
+	this->weight = obj.weight;
+	this->count = obj.count;
+	this->produceDate = new Date(*obj.produceDate);
+	this->name = new string(*obj.name);
+
+	return *this;
+}
