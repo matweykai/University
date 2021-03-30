c2f5cc4a1337a814270f054ffc08d2962c4e661e
Matwey Ivanov <matveykai@list.ru>
2021-03-30 14:35:27 +0300

    Configured and tested app

--- Lab3_OOP/Lab3_OOP/main.cpp
+++ Lab3_OOP/Lab3_OOP/main.cpp
@@ -1,7 +1,6 @@
 #include "Func.h"
-#include "FoodDataBase.h"
-#include <windows.h>
 #include "Menu.h"
+#include <windows.h>
 
 using namespace std;
 
@@ -10,22 +9,9 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	vector<string>* t_vect = new vector<string>();
-	t_vect->push_back("t_12");
+	Menu menu;
 
-	Food* t_food = new Food(1, FoodType(0), 1, 1, new Date(1,1,1), new string("name"));
-	t_food->print(t_vect, 10);
-	cout << endl;
-
-	t_food->destroy();
-	t_food = new FoodExtra(1, FoodType(0), 1, 1, new Date(1, 1, 1), new string("name"), new Date(2, 2, 2));
-	t_food->print(t_vect, 10);
-	t_food->destroy();
-
-	delete t_vect;
-	//Menu menu;
-
-	//menu.startMenu();
+	menu.startMenu();
 
 	return 0;
 }
