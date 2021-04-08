8e71ad9070abe2ae63737f70f6a80fa214aa786e
Matwey Ivanov <matveykai@list.ru>
2021-04-08 17:51:27 +0300

    Changed logic in functions, now they use STL algorithms

--- Lab7_OOP/Func.cpp
+++ Lab7_OOP/Func.cpp
@@ -1,29 +1,32 @@
 #include "Func.h"
 
+
+class Greater
+{
+public:
+	bool operator() (Food f1, Food f2)
+	{
+		return f1.get_price() > f2.get_price();
+	}
+};
+
 Food* find_by_date(deque<Food> arr, Date date) 
 {
-	for (deque<Food>::iterator iter = arr.begin(); iter < arr.end(); iter++)
-		if (*iter->get_date() == date)
-			return new Food(*iter);
+	deque<Food>::iterator result = find_if(arr.begin(), arr.end(), [date](Food obj) { return *obj.get_date() == date; });
 
-	return nullptr;
+	if (result != arr.end())
+		return new Food(*result);
+	else
+		return nullptr;
 }
 deque<Food> sort_by_price(deque<Food> arr) 
 {
 	deque<Food> result;
-	
-	int size = arr.size();
+	result.resize(arr.size());
 
-	for (int i = 0; i < size; i++)
-	{
-		deque<Food>::iterator max = arr.begin();
-		for (deque<Food>::iterator iter = arr.begin(); iter < arr.end(); iter++)
-			if (iter->get_price() > max->get_price())
-				max = iter;
-		
-		result.push_back(*max);
-		arr.erase(max);
-	}
+	copy(arr.begin(), arr.end(), result.begin());
+
+	sort(result.begin(), result.end(), Greater());
 
 	return result;
 }
--- Lab7_OOP/Func.h
+++ Lab7_OOP/Func.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "FoodDataBase.h"
+#include <algorithm>
 
 Food* find_by_date(deque<Food> arr, Date date);
 deque<Food> sort_by_price(deque<Food> arr);
