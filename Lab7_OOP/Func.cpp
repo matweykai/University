ffdfa8a85a52c7d59089a126fd4ea0476ae1202d
Matwey Ivanov <matveykai@list.ru>
2021-04-08 19:03:10 +0300

    Added functions for text working

--- Lab7_OOP/Func.cpp
+++ Lab7_OOP/Func.cpp
@@ -10,14 +10,20 @@ public:
 	}
 };
 
-Food* find_by_date(deque<Food> arr, Date date) 
+deque<Food> find_by_date(deque<Food> arr, Date date) 
 {
-	deque<Food>::iterator result = find_if(arr.begin(), arr.end(), [date](Food obj) { return *obj.get_date() == date; });
+	deque<Food>::iterator temp_obj = find_if(arr.begin(), arr.end(), [date](Food obj) { return *obj.get_date() == date; });
 
-	if (result != arr.end())
-		return new Food(*result);
-	else
-		return nullptr;
+	deque<Food> result;
+
+	while (temp_obj != arr.end())
+	{
+		result.push_back(*temp_obj);
+		temp_obj++;
+		temp_obj = find_if(temp_obj, arr.end(), [date](Food obj) { return *obj.get_date() == date; });
+	}
+
+	return result;
 }
 deque<Food> sort_by_price(deque<Food> arr) 
 {
@@ -29,4 +35,31 @@ deque<Food> sort_by_price(deque<Food> arr)
 	sort(result.begin(), result.end(), Greater());
 
 	return result;
+}
+void print_line(int size, int adding_count)
+{
+	for (int i = 0; i < size + adding_count; i++)
+		cout << "-";
+}
+void show_table(deque<Food> arr)
+{
+	cout.setf(ios::left);
+	cout.width(Food::get_maxLen() + NAME_SPACE);
+	cout << "Название";
+	cout.width(PRICE_S);
+	cout << "Цена";
+	cout.width(DATE_S);
+	cout << "Дата" << endl;
+	
+
+	print_line(Food::get_maxLen() + DATE_S + PRICE_S + NAME_SPACE, 2);
+	cout << endl;
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	print_line(Food::get_maxLen() + DATE_S + PRICE_S + NAME_SPACE, 2);
+	cout << endl;
 }
--- Lab7_OOP/Func.h
+++ Lab7_OOP/Func.h
@@ -2,5 +2,7 @@
 #include "FoodDataBase.h"
 #include <algorithm>
 
-Food* find_by_date(deque<Food> arr, Date date);
+deque<Food> find_by_date(deque<Food> arr, Date date);
 deque<Food> sort_by_price(deque<Food> arr);
+void print_line(int size, int adding_count = 0);
+void show_table(deque<Food> arr);
