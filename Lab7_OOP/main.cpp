31716fc77dfb76f15173f671ec47e8b49164b7bf
Matwey Ivanov <matveykai@list.ru>
2021-04-09 12:04:43 +0300

    Configured project. It's ready for testing

--- Lab7_OOP/main.cpp
+++ Lab7_OOP/main.cpp
@@ -1,7 +1,14 @@
-#include "Date.h"
-#include "FoodDataBase.h"
+#include "Menu.h"
+#include <windows.h>
 
 int main() 
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Menu menu("test.txt");
+
+	menu.startMenu();
+
 	return 0;
 }
